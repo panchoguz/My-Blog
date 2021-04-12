eb8a4def7f85bab25fcd243fb4f862d1558cb551
Francisco Guzmán <82102723+panchoguz@users.noreply.github.com>
2021-04-12 16:22:05 -0400

    data mad

--- _posts/2021-04-12-maddison-project-database-2020/auxgraph.R
+++ _posts/2021-04-12-maddison-project-database-2020/auxgraph.R
@@ -1,6 +1,4 @@
-my_palette = c("#DA0000", "#239f40", "#C4961A", "#CC79A7", "#C3D7A4", "#293352")
-
-ggplot(df_graf1, aes(year, gdppc, color=country)) +
+ggplot(data = df_graf1, aes(year, gdppc, color=country)) +
   geom_line(size = 1) +
   labs(title = "International Comparison of Real GDP per capita in 2011$",
        subtitle = "1800-2018",
@@ -13,11 +11,14 @@ ggplot(df_graf1, aes(year, gdppc, color=country)) +
   scale_y_continuous(labels = dollar) +
   theme_tufte() +
   theme(plot.title = element_text(size = rel(1.2)),
-        plot.subtitle = element_text(size = rel(1), margin=margin(0,0,20,0)),
+        plot.subtitle = element_text(size = rel(1),
+                                     margin= unit(0.1, "cm")),
         plot.caption = element_text(hjust = 0, face= "italic"),
-        axis.text.x = element_text(angle = 90, vjust=.5, size = 8),
-        axis.text.y = element_text(angle = 0, vjust=0, size = 8),
-        axis.ticks.x = element_line(size = 0),
+        axis.text.x = element_text(angle = 90, vjust=.5, size = 10,
+                                   margin = margin(10, 0, 10, 0)),
+        axis.text.y = element_text(angle = 0, vjust=0, size = 10),
+        axis.ticks.length.x = unit(.25, "cm"),
+        axis.ticks.x = element_line(size = .5),
         axis.ticks.y = element_blank(),
         panel.grid.major.y = element_line(colour = "grey90"),
         panel.grid.minor.y = element_line(colour = "grey90"))
